0fd14e91dedede3d7f7ed1c8e30d1f71bb1ebfe4
ChoiSungmin <76083571+csm5099@users.noreply.github.com>
2022-08-01 19:39:24 +0900

    joystick

--- week04/04_05_algorithm.py
+++ week04/04_05_algorithm.py
@@ -1,24 +1,21 @@
-def solution(progresses, speeds):
+def solution(name):
+    answer = 0
+    # minimum control count
+    min_move = len(name) - 1
     
-    answer = []
-    
-    while progresses != []:
+    for i, char in enumerate(name):
+    	# calcurate minimum control count
+        answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
         
-        #daily progress
-        for i, s in enumerate(zip(progresses, speeds)):
-            progresses[i] = sum(s)
-
-        #release
-        release = 0
-        while progresses[0] >= 100:
-            del progresses[0]
-            del speeds[0]
-            release += 1
-            if progresses == [] and speeds ==[]:
-                break
-        
-        # append released projects count
-        if release > 0:
-            answer.append(release)
+        # searching 'A'
+        next = i + 1
+        while next < len(name) and name[next] == 'A':
+            next += 1
+        print(next)
             
+        # 기존, 연속된 A의 왼쪽시작 방식, 연속된 A의 오른쪽시작 방식 비교 및 갱신
+        print(f"{i} {min_move} {2*i+len(name) - next} {i+2*(len(name) -next)}")
+        min_move = min([min_move, 2*i+len(name) - next, i+2*(len(name) -next)])
+        
+    answer += min_move
     return answer
