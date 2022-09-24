a2f4a8096dfdcbe7c2fe4340bf8cfe18ca6c3702
ChoiSungmin <76083571+csm5099@users.noreply.github.com>
2022-09-25 06:29:02 +0900

    2nd

---
+++ season/두 큐 합 같게 만들기.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def solution(queue1, queue2):
+    
+    q1 = deque(queue1)
+    q2 = deque(queue2)
+    
+    sum1 = sum(q1)
+    sum2 = sum(q2)
+    
+    for cnt in range(len(q1)*3):
+        if sum1 == sum2:
+            return cnt
+        if sum1 > sum2:
+            tmp = q1.pop(0)
+            q2.append(tmp)
+            sum1 -= tmp
+            sum2 += tmp
+        else:
+            tmp = q2.pop(0)
+            q1.append(tmp)
+            sum2 -= tmp
+            sum1 += tmp
+    return -1
